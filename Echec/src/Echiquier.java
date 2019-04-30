d8e4d2fde28f96389e222cf5915b061ff6336f14
Saied.S <saied.suroth@gmail.com>
2019-04-30 17:52:49 +0200

    Constructeur echiquier
    test initialiation un debut

--- Echec/src/Echiquier.java
+++ Echec/src/Echiquier.java
@@ -1,14 +1,36 @@
 public class Echiquier {
 	
-	private Case plateau[];
+	private Case[][] plateau;
 
-	public Case[] getPlateau() {
+	public Echiquier() {
+		plateau = new Case[8][8];
+		for(int i = 1 ; i <= 8 ; i++)
+		{
+			for(int j = 1 ; j <=8 ; j++)
+			{
+				plateau[i][j] = new Case();
+			}
+		}
+	}
+	
+	
+	public Case[][] getPlateau() {
 		return this.plateau;
 	}
 
-	public void setPlateau(Case[] plateau) {
+	public void setPlateau(Case[][] plateau) {
 		this.plateau = plateau;
 	}
 	
-	
+	public String toString() {
+		String s = null;
+		for(int i = 1 ; i <= 8 ; i++)
+		{
+			for(int j = 1 ; j <=8 ; j++)
+			{
+				s = this.plateau[i][j].toString();
+			}
+		}
+		return s;		
+	}
 }
