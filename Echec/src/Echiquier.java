49cb4a35be7433358312cc4b641c7e297b0aafb4
Saied.S <saied.suroth@gmail.com>
2019-05-02 19:45:03 +0200

    toutes les pieces instancié avec variable
    au cas où les pièces instanciés avec variable pour avoir un accès direct avec les pièces (en commentaire pour l'instant)

--- Echec/src/Echiquier.java
+++ Echec/src/Echiquier.java
@@ -4,6 +4,27 @@ public class Echiquier {
 
 	public Echiquier() {
 		this.plateau = new Case[8][8];
+//		Piece t1n = new Tour("noir");
+//		Piece t2n = new Tour("noir");
+//		Piece c1n = new Cavalier("noir");
+//		Piece c2n = new Cavalier("noir");
+//		Piece f1n = new Fou("noir");
+//		Piece f2n = new Fou("noir");
+//		Piece r1n = new Roi("noir");
+//		Piece d1n = new Reine("noir");
+//		Piece p1n = new Pion("noir"); Piece p2n = new Pion("noir"); Piece p3n = new Pion("noir"); Piece p4n = new Pion("noir"); 
+//		Piece p5n = new Pion("noir"); Piece p6n = new Pion("noir"); Piece p7n = new Pion("noir"); Piece p8n = new Pion("noir");
+//		
+//		Piece t1b = new Tour("blanc");
+//		Piece t2b = new Tour("blanc");
+//		Piece c1b = new Cavalier("blanc");
+//		Piece c2b = new Cavalier("blanc");
+//		Piece f1b = new Fou("blanc");
+//		Piece f2b = new Fou("blanc");
+//		Piece r1b = new Roi("blanc");
+//		Piece d1b = new Reine("blanc");
+//		Piece p1b = new Pion("blanc"); Piece p2b = new Pion("blanc"); Piece p3b = new Pion("blanc"); Piece p4b = new Pion("blanc"); 
+//		Piece p5b = new Pion("blanc"); Piece p6b = new Pion("blanc"); Piece p7b = new Pion("blanc"); Piece p8b = new Pion("blanc");
 		for(int i = 0 ; i <= 7 ; i++)
 		{
 			for(int j = 0 ; j <=7 ; j++)
