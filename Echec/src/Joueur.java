596a5b382d4aac6f94943dc488a17963a4a66fd7
Saied.S <saied.suroth@gmail.com>
2019-05-12 18:30:39 +0200

    test git a 2
    blabloubli

--- Echec/src/Joueur.java
+++ Echec/src/Joueur.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class Joueur {
 	
+	public void choixDeplacement(Echiquier echec)
+	{
+		String depart;
+		String arrive;
+		
+		Scanner choix = new Scanner(System.in);
+		System.out.println("Veuillez choisir la pièce que vous voulez déplacer (exemple : A6)\n");
+		depart = choix.nextLine();
+		while(depart.charAt(0) >= 'A' && depart.charAt(0) <= 'H')
+		{
+			if(depart.charAt(1) >= '0' && depart.charAt(1) <= '7')
+			{
+				System.out.println("Choix non conforme");
+			}
+		}
+	}
 }
