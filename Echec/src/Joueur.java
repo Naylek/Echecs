60f66ed3420704e884612e064ec54f13a211325e
Saied.S <saied.suroth@gmail.com>
2019-05-13 16:10:55 +0200

    getChoix Joueur
    retourner la case que le joueur a saisie auparavant avec les lettres + chiffres
    ça fait la conversion en gros

--- Echec/src/Joueur.java
+++ Echec/src/Joueur.java
@@ -19,7 +19,7 @@ public class Joueur {
 	
 	public String saisieControle(Echiquier echec)
 	{
-		String deplac; // le choix de la piece qu'il veut deplacer
+		String deplac; // le choix de la case selon l'affichage sur le terminale
 		Scanner choix = new Scanner(System.in);
 		deplac = choix.nextLine();	
 		
@@ -32,16 +32,38 @@ public class Joueur {
 		return deplac;
 	}
 	
+	public Case getChoix(String choix, Echiquier echec)// retourn la case choisie selon la saisie de l'utilisateur
+	{
+		char c = 'A'; // quand on increment on a la prochaine lettre
+		char l = '0'; // pareille prochain chiffre
+		for(int i = 0 ; i <= 7 ; i++) 
+		{
+			if(choix.charAt(0) == c) // verifie si le premier caractere est egale à char c sinon on increment char c 
+			{
+				for(int j = 0 ; j <= 7 ; j++) // si sa rentre dedans c'est quand on a trouvé l'indice de la premiere lettre dans l'éechiquier
+				{
+					if(choix.charAt(1) == l) // meme chose pour le deuxieme caractere
+					{
+						return echec.getCase(j, i); // l'indice j -> les lettre (ligne) | l'indiece i -> pour les chiffres (colonne)
+					}
+					l++; // '0' = '0' + '1' = '1' etc
+				}
+			}
+			c++; // pour regarder la prochaine lettre 'A' = 'A' + 1 = 'B' etc
+		}
+		return null; // vu que la saisie est verifié avant ça ne retournera jamais null
+		
+	}
+	
 	public void choixDeplacement(Echiquier echec)
 	{
 		String depart;
 		String arrive;
 		System.out.println("Veuillez choisir la pièce que vous voulez déplacer (exemple : A6)\n");
-		depart = this.saisieControle(echec);
-		System.out.println("Veuillez choisir la case que vous voulez\n");
-		arrive = this.saisieControle(echec);
-		System.out.println(depart);
-		System.out.println(arrive);
+		depart = this.saisieControle(echec).toUpperCase(); // on remet en majuscule car sinon ça revient en miniscule
+		//System.out.println("Veuillez choisir la case que vous voulez\n");
+		//arrive = this.saisieControle(echec).toUpperCase();
+		System.out.println(this.getChoix(depart, echec));
 	}
 		
 		
