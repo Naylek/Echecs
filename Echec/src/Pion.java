50d18e037323462bfc9e9d750fe9ad9659548416
Naylek <kelyan.baillargeau@gmail.com>
2019-06-15 14:21:16 +0200

    deplacementValide pion pour bouffer en diag

--- Echec/src/Pion.java
+++ Echec/src/Pion.java
@@ -15,18 +15,28 @@ public class Pion extends Piece {
 		}
 	}
 	
+	public boolean CouleurOpDiag(Echiquier echec, Case depart, Case arrive) { // verifie si une piece de couleure opposée est dans la diagonale+1
+		
+		Case dpt = echec.getCase(depart.getLigne(), depart.getColonne());
+		Case arr = echec.getCase(arrive.getLigne(), arrive.getColonne());
+		
+		if(depart.getColonne() == arrive.getColonne()+1 && arrive.caseOccupe("noir")) {
+			return true;
+		} else if (depart.getColonne() == arrive.getColonne()-1 && arrive.caseOccupe("noir")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
 	public boolean deplacementValide(Echiquier echec, Case depart, Case arrive) {
 		
 		Case dpt = echec.getCase(depart.getLigne(), depart.getColonne());
 		Case arr = echec.getCase(arrive.getLigne(), arrive.getColonne());
 		
-		/*
-		 * depart.getColonne() == arrive.getColonne()+1 && arrive.caseOccupe("noir")
-		 * depart.getColonne() == arrive.getColonne()-1 && arrive.caseOccupe("noir")
-		 */
-		
 		if(depart.caseOccupe("blanc")) {
-			if (depart.getColonne() == arrive.getColonne()) { //si meme colonne ou si pion couleur adverse dans diagonale direct
+			if (depart.getColonne() == arrive.getColonne() || this.CouleurOpDiag(echec, depart, arrive)) { //si meme colonne ou si pion couleur adverse dans diagonale direct
 				if (arr.caseOccupe() == false || arr.caseOccupe("noir")) { // si case d'arrivée vide OU contient piece noire 
 					if (dpt.getLigne() == 6) { // si 1er tour de pion
 						if (arr.getLigne() == dpt.getLigne() - 2 || arr.getLigne() == dpt.getLigne() - 1) { // si c 2 fois la case plus haute
