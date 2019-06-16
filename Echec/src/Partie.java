e030d69c95145cfb9752b32339a9fef4e822fe7c
Saied.S <saied.suroth@gmail.com>
2019-06-17 00:19:02 +0200

    Partie presque fini
    manque la condition while echec et mat

--- Echec/src/Partie.java
+++ Echec/src/Partie.java
@@ -1,85 +1,75 @@
 import java.util.Scanner;
 
 public class Partie {
+
+	public static boolean saisieControle(int choix)
+	{
+		if((choix >= 1 && choix <= 4))
+		{
+			return true;
+		}
+		return false;
+	}
 	
-	public void menu()
+	public static void main (String[] args)
 	{
 		Echiquier plateau = new Echiquier();
 		Joueur j1 = new Joueur(true); //blanc
 		Joueur j2 = new Joueur(false); //noir
 		int choix;
 		boolean tour = true;
-	
-		//while(//pas echec et mat joueur 1 || pas echec et mat joueur 2)
-//		{
-			if(tour == true)//tour joueur blanc
+		int i = 0;
+		while(i < 10)//pas echec et mat joueur 1 || pas echec et mat joueur 2)
+		{
+			System.out.println(plateau);
+			System.out.println("Vous voulez : 1 -> sauvegarder ? | 2 -> restaurer une partie ?  | 3 -> jouer ? | 4 -> arrêter la partie ?");
+			Scanner sc = new Scanner(System.in);
+			choix = sc.nextInt();
+			while(saisieControle(choix) == false)//saisie controlé
 			{
-				System.out.println(plateau);
-				System.out.println("Vous voulez : 1 -> sauvegarder ? | 2 -> restaurer une partie ?  | 3 -> jouer ?");
-				Scanner sc = new Scanner(System.in);
+				System.out.println("Saisie incorrect. Veuillez choisir un chiffre entre : [1-2-3-4]");
+				System.out.println("Vous voulez : 1 -> sauvegarder ? | 2 -> restaurer une partie ?  | 3 -> jouer ? | 4 -> arrêter la partie ?");
 				choix = sc.nextInt();
-				while(choix < 1 && choix > 3)
+			}
+			if(choix == 1)
+			{
+				if(tour == true)
 				{
-					System.out.println("Saisie incorrect. Veuillez choisir un chiffre entre : [1-2-3]");
-					System.out.println("Vous voulez : 1 -> sauvegarder ? | 2 -> restaurer une partie ?  | 3 -> jouer ?");
-					choix = sc.nextInt();
+					j1.sauver("sauvegarde.txt", plateau);//sauvegarde dans un fichier texte
 				}
-				if(choix == 1)
+				else if(tour == false)
 				{
-					if(tour == true)
-					{
-						j1.sauver("sauvegarde.txt", plateau);
-					}
-					else if(tour == false)
-					{
-						j2.sauver("sauvegarde.txt", plateau);
-					}
-				}
-				else if(choix == 2)
-				{
-					if(tour == true)
-					{
-						j1.charger("sauvegarde.txt", plateau);
-					}
-					else if(tour == false)
-					{
-						j2.charger("sauvegarde.txt", plateau);
-					}
-				}
-				else if(choix == 3)
-				{	
-					if(tour == true)
-					{
-						j1.choixDeplacement(plateau);//doit dire aussi que le joueur est en echec
-					}
-					else if(tour == false)
-					{
-						j2.choixDeplacement(plateau);
-					}
-					System.out.println(plateau);
-					tour = false;
+					j2.sauver("sauvegarde.txt", plateau);
 				}
 			}
-//		}
-	}
-	
-	public static void main (String[] args)
-	{
-		Echiquier plateau = new Echiquier();
-		Tour t = new Tour(false);
-		plateau.setCase(2, 5, t);
-		System.out.println(plateau);
-		Joueur j1 = new Joueur(true);
-		Joueur j2 = new Joueur(false);
-//		j1.sauver("sauvegarde.txt", plateau);
-		for(int i = 0; i < 10; i++)//pour faire plusieurs fois la saisie juste pour tester plus efficacement
-		{
-			j1.choixDeplacement(plateau);
-			System.out.println(plateau);
+			else if(choix == 2)
+			{
+				if(tour == true)
+				{
+					j1.charger("sauvegarde.txt", plateau);//charge par rapport a la sauvegarde effectuer
+				}
+				else if(tour == false)
+				{
+					j2.charger("sauvegarde.txt", plateau);
+				}
+			}
+			else if(choix == 3)
+			{	
+				if(tour == true)
+				{
+					j1.choixDeplacement(plateau);//doit dire aussi que le joueur est en echec
+				}
+				else if(tour == false)
+				{
+					j2.choixDeplacement(plateau);
+				}
+			}
+			else if(choix == 4)
+			{
+				break;
+			}
+			tour = false; //pour changer le tour
 		}
-//		j1.choixDeplacement(plateau);
-		
-//		j1.charger("sauvegarde.txt", plateau);
-//		System.out.println(plateau);
+		System.out.println("Fin de jeu.");
 	}
 }
